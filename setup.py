0ebee1b42c52080119d9e30635890e2a542917e2
Deepanshu03091995 <43910264+deepanshu03091995@users.noreply.github.com>
2024-02-23 01:39:33 +0530

    env file added to gitignore

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Deepanshu",
+    author_email="deepanshu.dlri@gmail.com",
+    packages=find_packages()
+)
